879d48e1c4abc04f2c040d7c3b1b404adec64262
boite <boite@users.noreply.github.com>
2017-04-09 15:56:43 +0100

    Move library from src/ to lib/ and set-up phpunit.

--- src/Command/GenerateCommand.php
+++ src/Command/GenerateCommand.php
@@ -6,8 +6,8 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Hl7v2\Message;
-use Hl7v2\Segment\MshSegment;
+//use Hl7v2\Message;
+//use Hl7v2\Segment\MshSegment;
 
 class GenerateCommand extends Command
 {
@@ -21,11 +21,11 @@ class GenerateCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        /*
         $message = new Message();
         $msh = new MshSegment();
         $message->addSegment($msh);
         print_r($message);
-        
-        
+        */
     }
 }
