1e239ed25cac81916b64707908b658b4023cb222
boite <boite@users.noreply.github.com>
2017-06-01 10:52:46 +0100

    Catch DataTypeError; improve message.

--- lib/Message/OruMessage.php
+++ lib/Message/OruMessage.php
@@ -4,6 +4,7 @@ namespace Hl7v2\Message;
 
 use Hl7v2\Encoding\Codec;
 use Hl7v2\Encoding\Datagram;
+use Hl7v2\Exception\DataTypeError;
 use Hl7v2\Exception\MessageError;
 use Hl7v2\Exception\SegmentError;
 use Hl7v2\Segment\ObrSegment;
@@ -33,17 +34,21 @@ class OruMessage extends AbstractMessage
         $patientGroup = null;
         $obrGroup = null;
 
+        $i = 1;
         while (false !== $codec->advanceToSegment($data)
             && false !== ($segmentId = $codec->extractSegmentId($data))
         ) {
+            ++$i;
             try {
                 $segment = $this
                     ->segmentFactory
                     ->create($segmentId, $data->getEncodingParameters())
                 ;
                 $segment->fromDatagram($data, $codec);
+            } catch (DataTypeError $e) {
+                throw new MessageError("Unable to decode segment {$i} ({$segmentId}) of ORU message. Error in Field.", null, $e);
             } catch (SegmentError $e) {
-                throw new MessageError('Unable to decode ORU message.', null, $e);
+                throw new MessageError("Unable to decode segment {$i} ({$segmentId}) of ORU message.", null, $e);
             }
 
             $this->segments[] = $segment;
