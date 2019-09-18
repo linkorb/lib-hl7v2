1adef4d22105e36ec28c895d8ace9f497eea83a6
boite <boite@users.noreply.github.com>
2019-09-18 14:27:32 +0100

    feat: add AdtMessage::fromDatagram

--- lib/Message/AdtMessage.php
+++ lib/Message/AdtMessage.php
@@ -4,6 +4,9 @@ namespace Hl7v2\Message;
 
 use Hl7v2\Encoding\Codec;
 use Hl7v2\Encoding\Datagram;
+use Hl7v2\Exception\DataTypeError;
+use Hl7v2\Exception\MessageError;
+use Hl7v2\Exception\SegmentError;
 
 /**
  * Patient Administration (ADT).
@@ -14,6 +17,24 @@ class AdtMessage extends AbstractMessage
 {
     public function fromDatagram(Datagram $data, Codec $codec)
     {
-        throw new \RuntimeException(__METHOD__ . ' is not implemented.');
+        $i = 1;
+        while (false !== $codec->advanceToSegment($data)
+            && false !== ($segmentId = $codec->extractSegmentId($data))
+        ) {
+            ++$i;
+            try {
+                $segment = $this
+                    ->segmentFactory
+                    ->create($segmentId, $data->getEncodingParameters())
+                ;
+                $segment->fromDatagram($data, $codec);
+            } catch (DataTypeError $e) {
+                throw new MessageError("Unable to decode segment {$i} ({$segmentId}) of ADT message. Error in Field.", null, $e);
+            } catch (SegmentError $e) {
+                throw new MessageError("Unable to decode segment {$i} ({$segmentId}) of ADT message.", null, $e);
+            }
+
+            $this->segments[] = $segment;
+        }
     }
 }
