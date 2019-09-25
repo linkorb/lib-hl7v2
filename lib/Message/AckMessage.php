556763679c7d4ed51924dc48717fc04f740951f0
boite <boite@users.noreply.github.com>
2019-09-25 13:15:29 +0100

    feat: add AckMessage::fromDatagram

--- lib/Message/AckMessage.php
+++ lib/Message/AckMessage.php
@@ -4,6 +4,9 @@ namespace Hl7v2\Message;
 
 use Hl7v2\Encoding\Codec;
 use Hl7v2\Encoding\Datagram;
+use Hl7v2\Exception\DataTypeError;
+use Hl7v2\Exception\MessageError;
+use Hl7v2\Exception\SegmentError;
 
 /**
  * Acknowledgement Message (ACK).
@@ -12,6 +15,24 @@ class AckMessage extends AbstractMessage
 {
     public function fromDatagram(Datagram $data, Codec $codec)
     {
-        throw new \RuntimeException(__METHOD__ . ' is not implemented.');
+        $i = 1;
+        while (false !== $codec->advanceToSegment($data)
+            && false !== ($segmentId = $codec->extractSegmentId($data))
+        ) {
+            ++$i;
+            try {
+                $segment = $this
+                    ->segmentFactory
+                    ->create($segmentId, $data->getEncodingParameters())
+                ;
+                $segment->fromDatagram($data, $codec);
+            } catch (DataTypeError $e) {
+                throw new MessageError("Unable to decode segment {$i} ({$segmentId}) of ACK message. Error in Field.", null, $e);
+            } catch (SegmentError $e) {
+                throw new MessageError("Unable to decode segment {$i} ({$segmentId}) of ACK message.", null, $e);
+            }
+
+            $this->segments[] = $segment;
+        }
     }
 }
